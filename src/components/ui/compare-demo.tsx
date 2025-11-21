4d8b00c757c57f130e072e5efd5e769de133e24c
Laksh <support@lakshpujary.me>
2025-11-21 20:46:32 +0530

    fix: swap compare demo images

--- src/components/ui/compare-demo.tsx
+++ src/components/ui/compare-demo.tsx
@@ -4,8 +4,8 @@ export function CompareDemo() {
     return (
         <div className="p-4 border rounded-3xl bg-purple-900/30 border-purple-800/50 px-4 backdrop-blur-sm">
             <Compare
-                firstImage="/code-after.png"
-                secondImage="/code-before.png"
+                firstImage="/code-before.png"
+                secondImage="/code-after.png"
                 firstImageClassName="object-cover object-left-top"
                 secondImageClassname="object-cover object-left-top"
                 className="h-[250px] w-[200px] md:h-[500px] md:w-[500px]"
